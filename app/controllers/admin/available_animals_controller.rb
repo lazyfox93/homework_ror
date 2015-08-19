8aa696c42f880fb70590b6aa05e83ea8e813615c
Yevgeniy Shapovalov <lazyfox93@gmail.com>
2015-08-19 14:24:47 +0300

    Add migartion in travis & empty available_animals_controller

--- app/controllers/admin/available_animals_controller.rb
+++ app/controllers/admin/available_animals_controller.rb
@@ -1,2 +1,27 @@
 class Admin::AvailableAnimalsController < ApplicationController
+  def new
+  	@available_animal = Available_animal.new
+  end
+  def create
+  	
+  end
+
+  def index
+
+  end
+  def show
+  	
+  end
+
+  def edit
+  	
+  end
+  def update
+
+  end
+
+  def destroy
+  	
+  end
+
 end
